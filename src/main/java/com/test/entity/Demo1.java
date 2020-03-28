c732aec965768242ae001e10bfa2f7f4e58d2c52
klevel_glb <kj_glb@qq.com>
2020-03-28 11:16:44 +0800

    这波是MavenProject

--- src/main/java/com/test/entity/Demo1.java
+++ src/main/java/com/test/entity/Demo1.java
@@ -2,9 +2,14 @@ package com.test.entity;
 
 public class Demo1 {
 
+	public void test() {
 		
-		public void test() {
+		System.out.println("这波啊，这波是");
+	}
+	
+	public void main() {
 		
-			System.out.println("这波啊，这波是");
-		}
-}
+		System.out.println("你在干什么");
+	}
+	
+}	
